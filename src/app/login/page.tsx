8bb7db5680ba97796d52d4b68b7d48d3647214b3
franco123rex777 <franco123rex777@gmail.com>
2025-01-23 02:10:08 +0000

    test

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,18 @@ import { getAccountInfo } from "./lib/tmdb"
 
 
 export default async function Login() {
-    const {username} = await getAccountInfo()
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NmE4NGE2YmZkMjU4NzU4YWU2NTg1MDVhYWViZmM4YSIsIm5iZiI6MTczNjc3NTA2NC44MjA5OTk5LCJzdWIiOiI2Nzg1MTU5OGM4MWFjYWE2M2RiYzA1MjQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.pKv3cNHvuvheHOY_2SgJS1UqN5LRoM_XfWB2W8mXVlw'
+        }
+      };
+      
+    const data = await fetch('https://api.themoviedb.org/3/account/21748492', options)
+    const json = await data.json()
+
+    const {username} = json
     console.log(username)
     return (
     <div className="flex gap-4">
